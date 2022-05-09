13173b488507b98aa5561def31c521f8ebcf7a9d
Daris02 <hei.raymond.2@gmail.com>
2022-05-09 12:55:33 -0700

    Projet v.2

--- code.js
+++ code.js
@@ -1,4 +1,27 @@
 //Getdocumet from index.html
-window.addEventListener("DOMContentLoaded", function(e){
-    alert("DOM charger");
+window.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM charger");
+    
+    const buttonE = document.getElementById("bnE");
+    const button$ = document.getElementById("bn$");
+    const valeur = document.getElementById("valeurR");
+
+    buttonE.addEventListener("click", function(){
+        if(valeur.value == 0 || valeur.value < 0){
+            alert("Attention: veullier saisir des valeur positif ou non nul, s'il vous plait");
+        }
+        else{
+            let result  = (valeur.value / 4264.).toFixed(2) ;
+            alert(valeur.value+" Ariary"+"  vos "+result+" Euro");
+    };
+});
+    button$.addEventListener("click", function(){
+        if(valeur.value == 0 || valeur.value < 0){
+            alert("Attention, veullier saisir des valeur positif ou non nul, s'il vous plait");
+        }
+        else{
+            let result  = (valeur.value / 4006).toFixed(2) ;
+            alert(valeur.value+" Ariary"+"  vos "+result+" $");
+        }
+    });
 });
